bfe18ae14766bf29a5be165b621fdc986de8bb30
Gamattowicz <p.romanczuk31@gmail.com>
2021-01-16 21:53:47 +0100

    add search function

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const inputAdd = document.querySelector('input.add')
 const btnAdd = document.querySelector('button.add');
 const ul = document.querySelector('ul');
-
+let number = 0;
+let listLi;
 
 //Function removing tasks from list by click to button 
 const rmvBtn = (e) => {
     e.target.parentElement.remove();
+    number--;
+    document.querySelector('h2 span').textContent = number;
+    listLi = [...document.querySelectorAll('li')];
 }
 
 //Function adding strikethrough
@@ -29,7 +33,28 @@ const addTask = (e) => {
     li.addEventListener('click', taskDone);
     li.appendChild(btnRmv)
     ul.appendChild(li);
+    number++;
+    document.querySelector('h2 span').textContent = number;
     inputAdd.value = '';
+    listLi = [...document.querySelectorAll('li')];
 }
 
 btnAdd.addEventListener('click', addTask);
+
+//Search function
+const inputSrc = document.querySelector('input.search')
+
+const searchTask = (e) => {
+    let result = listLi.filter(li =>
+        li.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+    )
+    console.log(result);
+    ul.textContent = '';
+    result.forEach(li => {
+        ul.appendChild(li)
+    })
+
+}
+
+
+inputSrc.addEventListener('input', searchTask);
